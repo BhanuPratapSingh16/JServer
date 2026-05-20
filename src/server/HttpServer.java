dbc3f3948d1d393b0792a7e3af68d7283677d802
Bhanu Pratap Singh <bhanuprataps1610@gmail.com>
2026-05-20 16:40:38 +0530

    Solved multithreading issue

--- src/server/HttpServer.java
+++ src/server/HttpServer.java
@@ -1,19 +1,21 @@
 package src.server;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer{
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(8080);
         System.out.println("Server started on port 8080");
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected");
             
-            ClientHandler.execute(clientSocket);
-
-            clientSocket.close();
+            threadPool.submit(new ClientHandler(clientSocket));
         }
     }
 }
